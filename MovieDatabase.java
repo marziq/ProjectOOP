6ca7bbace3538dd0927abd2dc1652c79678b2dab
Ammar Haziq <108142876+marziq@users.noreply.github.com>
2023-04-20 15:36:09 +0800

    Update MovieDatabase.java

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -4,71 +4,51 @@ import java.io.IOException;
 
 public class MovieDatabase {
     private Movie[] movies;
-    private int numMovies;
+    private int size;
     
     public MovieDatabase() {
-        movies = new Movie[100]; // Assuming there are no more than 100 movies in the database
-        numMovies = 0;
+        movies = new Movie[100]; // Initialize array with a capacity of 100
+        size = 0;
         
         try {
             // Create a FileReader object to read from a file
             FileReader fileReader = new FileReader("movies.csv");
             
-            // Create a BufferedReader object to read text from the FileReader
+            // Create a BufferedReader object to read lines of text from the FileReader
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             
-            // Read the first line of the file (assuming it contains headers)
-            String headers = bufferedReader.readLine();
-            
-            // Loop through the remaining lines of the file
             String line;
+            
+            // Read each line of text from the file
             while ((line = bufferedReader.readLine()) != null) {
-                // Split the line into separate values based on the comma delimiter
-                String[] values = line.split(",");
+                // Split the line into an array of strings based on the comma delimiter
+                String[] parts = line.split(",");
                 
-                // Create a new Movie object and add it to the movies array
-                Movie movie = new Movie(values[0], values[1], Integer.parseInt(values[2]), Double.parseDouble(values[3]));
-                movies[numMovies] = movie;
-                numMovies++;
+                // Create a new movie object using the data from the line
+                Movie movie = new Movie(parts[0], parts[1], parts[2], parts[3]);
+                
+                // Add the movie object to the movies array
+                movies[size] = movie;
+                size++;
             }
             
             // Close the BufferedReader
             bufferedReader.close();
         } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
+            System.out.println("Error reading file: " + e.getMessage());
         }
     }
     
-    public Movie[] search(String query) {
-        Movie[] results = new Movie[100]; // Assuming there are no more than 100 matches
+    public void searchMovies(String keyword) {
+        System.out.println("Search results:");
         
-        // Loop through the movies array and check each movie against the query
-        int numResults = 0;
-        for (int i = 0; i < numMovies; i++) {
-            Movie movie = movies[i];
-            
-            // Check if the movie title, genre, or director contains the query (case-insensitive)
-            if (movie.getTitle().toLowerCase().contains(query.toLowerCase()) ||
-                movie.getGenre().toLowerCase().contains(query.toLowerCase()) ||
-                movie.getDirector().toLowerCase().contains(query.toLowerCase())) {
-                
-                // If the movie matches the query, add it to the results array
-                results[numResults] = movie;
-                numResults++;
+        // Loop through each movie in the movies array
+        for (int i = 0; i < size; i++) {
+            // Check if the movie matches the keyword in either the title, genre, or director fields
+            if (movies[i].getTitle().contains(keyword) || movies[i].getGenre().contains(keyword) || movies[i].getDirector().contains(keyword)) {
+                // If the movie matches, display its details
+                System.out.println(movies[i].getTitle() + " (" + movies[i].getYear() + ") - " + movies[i].getGenre() + " - " + movies[i].getDirector() + " - " + movies[i].getRating());
             }
         }
-        
-        // If no matches were found, return null
-        if (numResults == 0) {
-            return null;
-        }
-        
-        // Otherwise, create a new array with the correct size and copy the matches into it
-        Movie[] finalResults = new Movie[numResults];
-        for (int i = 0; i < numResults; i++) {
-            finalResults[i] = results[i];
-        }
-        
-        return finalResults;
     }
 }
