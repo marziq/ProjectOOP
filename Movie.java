48c4f880858069e50089a226a6bfca5addf22c93
Ammar Haziq <108142876+marziq@users.noreply.github.com>
2023-05-22 10:27:46 +0800

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -1,4 +1,30 @@
 package Project;
 
 public class Movie extends MediaItem{
+    private String director;
+    private int duration;
+
+    Movie(){
+    }
+    Movie(String title, double rating, String genre, int yearRelease, String director, int duration){
+        super(title, rating, genre, yearRelease);
+        this.director = director;
+        this.duration = duration;
+    }
+    String getDirector(){
+        return director;
+    }
+    int getDuration(){
+        return duration;
+    }
+    void setDirector(String director){
+        this.director = director;
+    }
+    void setDuration(int duration){
+        this.duration = duration;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", Director: " + director + ", Duration(minutes): " + duration;
+    }
 }
