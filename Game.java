67fc4b1bbb1e04ff6726402dc0c9d7a310584aa4
Ammar Haziq <108142876+marziq@users.noreply.github.com>
2023-05-22 10:25:41 +0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,4 +1,40 @@
 package Project;
 
 public class Game extends MediaItem{
+    private String developer;
+    private String platform;
+    private static int gameCounts = 0;
+    public Game(){
+    }
+    public Game(String title, double rating, String genre, int year, String developer, String platform){
+        super(title,rating,genre,year);
+        this.developer = developer;
+        this.platform = platform;
+        gameCounts++;
+    }
+    public String getName(){
+        return getTitle();
+    }
+    public String searchGenre(){
+        return getGenre();
+    }
+    public String getDeveloper(){
+        return developer;
+    }
+    public String getPlatform(){
+        return platform;
+    }
+    public void setDeveloper(String developer){
+        this.developer = developer;
+    }
+    public void setPlatform(String platform){
+        this.platform = platform;
+    }
+    public static int getGameCounts(){
+        return gameCounts;
+   }
+    @Override
+    public String toString(){
+        return super.toString() + ", Developer: " + developer + ", Platform: " + platform;
+    }
 }
